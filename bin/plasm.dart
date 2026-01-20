af2d0a692900cd86989d7cb15203f2262b2c1b69
Ludwig Lehnert <ludwig@lehnert.dev>
2026-01-20 16:47:57 +0100

    initial commit

---
+++ bin/plasm.dart
@@ -0,0 +1,5 @@
+import 'package:plasm/plasm.dart' as plasm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${plasm.calculate()}!');
+}
---
+++ test/plasm_test.dart
@@ -0,0 +1,8 @@
+import 'package:plasm/plasm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
